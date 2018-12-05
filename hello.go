d234bd58e272947bb3548082b2f9362841e87fb0
Phillip Ankerson <phillip.ankerson@naom.local>
2018-12-05 15:45:41 -0600

    using library

--- hello.go
+++ hello.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/pankers/stringutil"
+)
 
 func main() {
 	fmt.Println("Hello world.")
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
 }
